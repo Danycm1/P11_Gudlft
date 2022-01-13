f915f55bfe0f36e251d7a58df65a72d38c21be73
Dany CM <danycmwow@gmail.com>
2022-01-13 17:56:03 +0100

    fix: Forgotten merge change

--- server.py
+++ server.py
@@ -76,7 +76,7 @@ def purchase_places():
         competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - places_required
         club['points'] = int(club['points']) - places_required
         flash('Great-booking complete!')
-    return render_template('welcome.html', club=club, competitions=competitions)
+    return render_template('welcome.html', club=club, competitions=competitions, date=current_date)
 
 
 @app.route('/displayPoints')
